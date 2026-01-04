70981684841bb45a758345c11cf26118797208a1
naveen <143981026+NAVEEN90530@users.noreply.github.com>
2026-01-04 06:43:40 +0530

    Refactor category API functions for clarity

--- mern-portfolio-frontend/api/category.js
+++ mern-portfolio-frontend/api/category.js
@@ -1,12 +1,10 @@
 import api from "./axios";
 
-// LIST ALL CATEGORIES
 export const listCategories = async () => {
   const { data } = await api.get("/categories");
   return data;
 };
 
-// LIST CATEGORIES BY DOMAIN
 export const listDomainCategories = async (domainId) => {
   const url = domainId
     ? `/categories?domain=${domainId}`
@@ -15,19 +13,16 @@ export const listDomainCategories = async (domainId) => {
   return data;
 };
 
-// CREATE CATEGORY
 export const createCategory = async (categoryData) => {
   const { data } = await api.post("/categories", categoryData);
   return data;
 };
 
-// UPDATE CATEGORY
 export const updateCategory = async (id, categoryData) => {
   const { data } = await api.put(`/categories/${id}`, categoryData);
   return data;
 };
 
-// DELETE CATEGORY
 export const deleteCategory = async (id) => {
   const { data } = await api.delete(`/categories/${id}`);
   return data;
