71b37854ad066b8d222f2077807c64b1c409e519
naveen <143981026+NAVEEN90530@users.noreply.github.com>
2026-01-04 06:35:32 +0530

    Update API base URL to use constant VITE_API_URL

--- mern-portfolio-frontend/api/api.js
+++ mern-portfolio-frontend/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const VITE_API_URL = "https://auroxdemo.onrender.com";
+
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: `${VITE_API_URL}/api`,
   withCredentials: true,
 });
 
