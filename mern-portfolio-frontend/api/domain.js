0afce9536358d094fd09291c2a4889bbe82d8137
naveen <143981026+NAVEEN90530@users.noreply.github.com>
2026-01-04 06:43:14 +0530

    Remove comments from domain.js

--- mern-portfolio-frontend/api/domain.js
+++ mern-portfolio-frontend/api/domain.js
@@ -1,24 +1,20 @@
 import api from "./axios";
 
-// LIST DOMAINS
 export const listDomains = async () => {
   const { data } = await api.get("/domains");
   return data;
 };
 
-// CREATE DOMAIN
 export const createDomain = async (domainData) => {
   const { data } = await api.post("/domains", domainData);
   return data;
 };
 
-// UPDATE DOMAIN
 export const updateDomain = async (id, domainData) => {
   const { data } = await api.put(`/domains/${id}`, domainData);
   return data;
 };
 
-// DELETE DOMAIN
 export const deleteDomain = async (id) => {
   const { data } = await api.delete(`/domains/${id}`);
   return data;
